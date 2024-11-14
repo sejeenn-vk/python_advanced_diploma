3ef9e70d111161a41b815f42664d6f5c25811d01
Евгений Воронцов <sejeenn@gmail.com>
2024-11-14 15:01:45 +0300

    Создана директория бэкенда src c файлом main.py

---
+++ src/main.py
@@ -0,0 +1,19 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="../static"), name="static")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def get_main():
+    with open("../static/index.html", "r", encoding="utf-8") as file:
+        index_html = file.read()
+    return HTMLResponse(content=index_html)
+
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", host='0.0.0.0', port=8000, reload=True)
