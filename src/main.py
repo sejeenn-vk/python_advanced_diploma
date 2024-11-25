c0b2498272e74e65d84d74986fc7a291efe28374
Eugene Vorontsov <sejeenn@gmail.com>
2024-11-25 21:18:42 +0300

    Проверено создание таблиц

--- src/main.py
+++ src/main.py
@@ -1,6 +1,18 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from src.api.users_me import users_me
-from src.core.models.db_helper import db_helper
 
-app = FastAPI()
-app.include_router(users_me)
+from src.core.models.db_helper import db_helper
+from src.core.models.base import Base
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with db_helper.engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+    await db_helper.dispose()
+
+main_app = FastAPI(lifespan=lifespan)
+main_app.include_router(users_me)
