03b70ac99e7253e2d2d90c717a17c33fbbc060b1
Евгений Воронцов <sejeenn@gmail.com>
2024-11-25 16:49:59 +0300

    Пытаюсь запустить подключение к БД с помощью db_helper

---
+++ src/core/models/__init__.py
@@ -0,0 +1,5 @@
+__all__ = (
+    "db_helper",
+)
+
+from src.core.models.db_helper import db_helper
--- src/main.py
+++ src/main.py
@@ -1,11 +1,6 @@
-import uvicorn
 from fastapi import FastAPI
 from src.api.users_me import users_me
-
+from src.core.models.db_helper import db_helper
 
 app = FastAPI()
 app.include_router(users_me)
-
-
-# if __name__ == "__main__":
-#     uvicorn.run("main:app", host="0.0.0.0", port=8000)
