0e54bf17cdf90d282bf7f39515b312779f6d1d42
Евгений Воронцов <sejeenn@gmail.com>
2024-11-26 17:02:01 +0300

    Сделан post запрос по созданию пользователя

--- src/api/users_me.py
+++ src/api/users_me.py
@@ -1,8 +1,10 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Header
-
-from src.core.schemas.users import UsersMe
+from fastapi import APIRouter, Header, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from src.core.schemas.users import UsersMe, CreateUser
+from src.core.models.db_helper import db_helper
+from src.api.crud import user as users_crud
 
 users_me = APIRouter(
     prefix="/api"
@@ -21,3 +23,15 @@ async def get_users_me(api_key: Annotated[str | None, Header()] = None):
             "following": [{"id": 3, "name": "Татьяна Воронцова"}],
         },
     }
+
+
+@users_me.post("", response_model=CreateUser)
+async def create_user(
+        session: Annotated[
+            AsyncSession,
+            Depends(db_helper.session_getter),
+        ],
+        user_create: CreateUser,
+):
+    user = await users_crud.create_user(session=session, user_create=user_create)
+    return user
