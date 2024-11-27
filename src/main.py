55bd4ff233220813dd397dea432fd7ead2e3272c
Евгений Воронцов <sejeenn@gmail.com>
2024-11-27 16:02:45 +0300

    Исправлен main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
-from src.api.users_me import users_me
+from src.api.users import users_route
 
 from src.core.models.db_helper import db_helper
 from src.core.models.base import Base
@@ -9,10 +9,10 @@ from src.core.models.base import Base
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # async with db_helper.engine.begin() as conn:
-        # await conn.run_sync(Base.metadata.drop_all)
-        # await conn.run_sync(Base.metadata.create_all)
+    #     await conn.run_sync(Base.metadata.drop_all)
+    #     await conn.run_sync(Base.metadata.create_all)
     yield
     # await db_helper.dispose()
 
 main_app = FastAPI(lifespan=lifespan)
-main_app.include_router(users_me)
+main_app.include_router(users_route)
