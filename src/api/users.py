51501ab30b1198378cffec707a3a27578d837970
Евгений Воронцов <sejeenn@gmail.com>
2024-11-27 16:05:01 +0300

    сделано получение followers, following исправлен роут создания пользователя

--- src/api/users.py
+++ src/api/users.py
@@ -2,16 +2,16 @@ from typing import Annotated
 
 from fastapi import APIRouter, Header, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
-from src.core.schemas.users import UsersMe, CreateUser
+from src.core.schemas.users import UsersMe, CreateUser, UserRead
 from src.core.models.db_helper import db_helper
 from src.api.crud import user as users_crud
 
-users_me = APIRouter(
+users_route = APIRouter(
     prefix="/api"
 )
 
 
-@users_me.get("/users/me", tags=["Users me"], response_model=UsersMe)
+@users_route.get("/users/me", tags=["Users me"], response_model=UsersMe)
 async def get_users_me(
         session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
         api_key: Annotated[str | None, Header()] = None,
@@ -19,15 +19,17 @@ async def get_users_me(
     user = await users_crud.get_user_by_api_key(session=session, api_key=api_key)
     data = {"result": "true", "user": {
         "id": user.id, "name": user.name,
-        "followers": [], "following": []
+        "followers": [{"id": x.id, "name": x.name} for x in user.followers],
+        "following": [{"id": x.id, "name": x.name} for x in user.followed]
     }}
-
-    # print(f"_________________{user.followers}________________________________")
-
     return data
 
 
-@users_me.post("", response_model=CreateUser)
+@users_route.post(
+    "/users/me",
+    tags=["Создать нового пользователя"],
+    response_model=UserRead
+)
 async def create_user(
         session: Annotated[
             AsyncSession,
