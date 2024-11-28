57f8474da4062d0a66b6ba4753756fc1268092d0
Евгений Воронцов <sejeenn@gmail.com>
2024-11-28 16:32:14 +0300

    Создан роут для получения или отправки твитов

--- src/api/tweet.py
+++ src/api/tweet.py
@@ -14,76 +14,64 @@ tweets_route = APIRouter(
 @tweets_route.post(
     "/tweets"
 )
-async def create_user(
+async def create_new_tweet(
         session: Annotated[
             AsyncSession,
             Depends(db_helper.session_getter),
         ],
-        # tweet_create: CreateTweet,
+        # tweet_create: str = Header(),
 ):
-
+    # print(tweet_create.encode())
     return {"result": "true", "tweet_id": 100500}
 
 
 @tweets_route.get(
     "/tweets"
 )
-async def create_user(
+async def get_all_tweets(
         session: Annotated[
             AsyncSession,
             Depends(db_helper.session_getter),
         ],
         # tweet_create: CreateTweet,
 ):
-
+    # /api/tweets?offset=1&limit=5 роут с пагинацией
     return {
         "result": "true",
         "tweets": [
             {
                 "id": 100,
                 "content": "Жили у бабуси, два весёлых гуся!",
-                "attachments": []
+                "attachments": [],
+                "author": {
+                    "id": 2,
+                    "name": "Евгений Онегин",
+                },
+                "likes": [
+                    {
+                        "user_id": 1,
+                        "name": "Николай Гоголь",
+                    },
+                    {
+                        "user_id": 4,
+                        "name": "Владимир Ульянов",
+                    }
+                ]
             },
             {
                 "id": 101,
                 "content": "Один серый другой белый, два веселых гуся!",
-                "attachments": []
+                "attachments": [],
+                "author": {
+                    "id": 5,
+                    "name": "Александр Пушкин",
+                },
+                "likes": [
+                    {
+                        "user_id": 4,
+                        "name": "Владимир Ульянов"
+                    }
+                ]
             }
         ],
-        "author": {
-            "id": 5,
-            "name": "George Bush",
-        },
-        "likes": [
-            {
-                "user_id": 1,
-                "name": "Bill Clinton"
-            }
-        ]
     }
-
-# {
-# “result”: true,
-# "tweets": [
-# {
-# "id": int,
-# "content": string,
-# "attachments" [
-# link_1, // relative?
-# link_2,
-# ...
-# ]
-# "author": {
-# "id": int
-# "name": string
-# }
-# “likes”: [
-# {
-# “user_id”: int,
-# “name”: string
-# }
-# ]
-# },
-# ...,
-# ]
-# }
