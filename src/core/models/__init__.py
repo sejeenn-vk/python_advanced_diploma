34aec64c9a2885918da876a353e173c843635981
Евгений Воронцов <sejeenn@gmail.com>
2024-11-28 16:32:28 +0300

    Добавлены новые модели

--- src/core/models/__init__.py
+++ src/core/models/__init__.py
@@ -2,8 +2,14 @@ __all__ = (
     "db_helper",
     "Base",
     "User",
+    "Tweet",
+    "Like",
+    "Image",
 )
 
 from src.core.models.db_helper import db_helper
-from src.core.models.base import Base
-from src.core.models.user import User
+from .base import Base
+from .user import User
+from .tweet import Tweet
+from .likes import Like
+from .images import Image
