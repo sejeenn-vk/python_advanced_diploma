9628de8b5a67131a99acd90803fb8b8a20e94116
Евгений Воронцов <sejeenn@gmail.com>
2024-11-29 16:35:53 +0300

    Созданы crud и роуты твитов (создание и получение)

--- src/api/crud/tweet.py
+++ src/api/crud/tweet.py
@@ -1,8 +1,49 @@
+from sqlalchemy import select
+from sqlalchemy.orm import joinedload
+
 from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.core.models import User, Tweet, Like
+from src.core.schemas.tweet import CreateTweetSchema
 
 
 async def create_new_tweet(
         session: AsyncSession,
+        tweet_data: CreateTweetSchema,
+        current_user: User,
+) -> Tweet:
+    """
+    Создание нового твита в базе данных.
+    :param session: AsyncSession
+    :param tweet_data: schema CreateTweet
+    :param current_user: model User
+    :return:
+    """
+    tweet = Tweet(content=tweet_data.tweet_data, user_id=current_user.id)
+    session.add(tweet)
+    await session.commit()
+    return tweet
 
+
+async def get_all_tweets(
+        session: AsyncSession,
+        current_user: User,
 ):
-    return
+    """
+    Пользователь может получить ленту из твитов отсортированных в
+    порядке убывания по популярности от пользователей, которых он
+    читает.
+    :param current_user:
+    :param session:
+    :return:
+    """
+    result = await session.execute(
+        select(Tweet)
+        .options(
+            joinedload(Tweet.user),
+            joinedload(Tweet.likes).subqueryload(Like.user),
+            joinedload(Tweet.images),
+        )
+        .order_by(Tweet.created_at.desc())
+    )
+    return result.scalars().unique().all()
--- src/api/tweet.py
+++ src/api/tweet.py
@@ -5,9 +5,9 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.core.models import db_helper, User
-from src.core.schemas.tweet import CreateTweet, TweetResponse
+from src.core.schemas.tweet import CreateTweetSchema, TweetResponse, TweetListSchema
 from src.utils.get_user import get_current_user
-from src.api.crud.tweet import create_new_tweet
+from src.api.crud import tweet as tweet_crud
 
 tweets_route = APIRouter(
     prefix="/api/tweets"
@@ -20,68 +20,30 @@ async def create_new_tweet(
             AsyncSession,
             Depends(db_helper.session_getter),
         ],
-        tweet: CreateTweet,
+        tweet_data: CreateTweetSchema,
         current_user: Annotated[User, Depends(get_current_user)],
 
 ):
-
     # Пишем твит
-    tweet = await create_new_tweet(
-        tweet=tweet, current_user=current_user, session=session
+    tweet = await tweet_crud.create_new_tweet(
+        tweet_data=tweet_data, current_user=current_user, session=session
     )
-    print(tweet)
     return {"tweet_id": tweet.id}
-
-    # return {"result": "true", "tweet_id": 100500}
 
 
 @tweets_route.get(
-    ""
+    "",
+    response_model=TweetListSchema,
+    status_code=200,
 )
 async def get_all_tweets(
         session: Annotated[
             AsyncSession,
             Depends(db_helper.session_getter),
         ],
-        # tweet_create: CreateTweet,
+        current_user: Annotated[User, Depends(get_current_user)],
 ):
     # /api/tweets?offset=1&limit=5 роут с пагинацией
-    return {
-        "result": "true",
-        "tweets": [
-            {
-                "id": 100,
-                "content": "Жили у бабуси, два весёлых гуся!",
-                "attachments": [],
-                "author": {
-                    "id": 2,
-                    "name": "Евгений Онегин",
-                },
-                "likes": [
-                    {
-                        "user_id": 1,
-                        "name": "Николай Гоголь",
-                    },
-                    {
-                        "user_id": 4,
-                        "name": "Владимир Ульянов",
-                    }
-                ]
-            },
-            {
-                "id": 101,
-                "content": "Один серый другой белый, два веселых гуся!",
-                "attachments": [],
-                "author": {
-                    "id": 5,
-                    "name": "Александр Пушкин",
-                },
-                "likes": [
-                    {
-                        "user_id": 4,
-                        "name": "Владимир Ульянов"
-                    }
-                ]
-            }
-        ],
-    }
+    tweets = await tweet_crud.get_all_tweets(session=session, current_user=current_user)
+    print(tweets)
+    return {"tweets": tweets}
