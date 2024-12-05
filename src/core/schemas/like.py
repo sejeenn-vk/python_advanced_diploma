801dbfd8c660f93ce377605f1d69cd6d755a90a3
Евгений Воронцов <sejeenn@gmail.com>
2024-12-05 16:32:37 +0300

    добавлен валидатор модели в схему, для подгрузки данных пользователя

--- src/core/schemas/like.py
+++ src/core/schemas/like.py
@@ -13,3 +13,13 @@ class LikeSchema(BaseModel):
         from_attributes=True,
         populate_by_name=True,  # Использовать псевдоним вместо названия поля
     )
+
+    @model_validator(mode="before")
+    def extract_user(cls, data):
+        """
+        Метод извлекает и возвращает данные о пользователе из объекта Like
+        """
+        # ВАЖНО: доступ к данным пользователя возможен благодаря связыванию данных при SQL-запросе к БД
+        # при выводе твитов - joinedload(Tweet.likes).subqueryload(Like.user)
+        user = data.user
+        return user
