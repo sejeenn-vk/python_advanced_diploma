7d8400b6b28a80b352d5267229eabc69c760e09c
Евгений Воронцов <sejeenn@gmail.com>
2024-12-05 16:30:58 +0300

    сделана репрезентация модели лайков

--- src/core/models/likes.py
+++ src/core/models/likes.py
@@ -11,7 +11,10 @@ class Like(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
-    tweets_id: Mapped[int] = mapped_column(ForeignKey("tweets.id"))
+    tweet_id: Mapped[int] = mapped_column(ForeignKey("tweets.id"))
 
+    def __repr__(self):
+        return (f"Like(id={self.id}, user_id={self.user_id}, "
+                f"tweet_id={self.tweet_id})")
     # Отключаем проверку строк, тем самым убирая уведомление, возникающее при удалении несуществующей строки
     __mapper_args__ = {"confirm_deleted_rows": False}
